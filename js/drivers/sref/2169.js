f0fcf2548ba0f9c18cb276ab4434dccbb2c39183
Gary van Breda <gary.h@vbitservices.com>
2020-01-23 00:07:21 +0000

    Add 2169 Sref Driver

---
+++ js/drivers/sref/2169.js
@@ -0,0 +1,43 @@
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ */
+
+/**
+ * A driver to provide EPSG:2169 (LUREF) specific functions.
+ */
+
+if (typeof indiciaData.srefHandlers==="undefined") {
+  indiciaData.srefHandlers={};
+}
+
+indiciaData.srefHandlers['2169'] = {
+
+  srid: 2169,
+
+  returns: ['gridNotation'],
+
+  /**
+   * Format an x, y into a LUREF 
+   */
+  pointToGridNotation: function(point, digits) {
+    var precision = 5 - (digits/2),
+        x = Math.floor(point.x / Math.pow(10,precision)),
+        y = Math.floor(point.y / Math.pow(10,precision));
+    if(precision) {
+    	x = (x + 0.5) * Math.pow(10,precision);
+    	y = (y + 0.5) * Math.pow(10,precision);
+    }
+    return x + ', ' + y;
+  }
+};
