d62e0b6fee93f72d47b7e7f8c4b658af2f12f68d
John van Breda <john.vanbreda@biodiverseit.co.uk>
2025-05-27 16:05:40 +0100

    Proxy now only for Google Place Search API

--- js/postcode_search.js
+++ js/postcode_search.js
@@ -20,7 +20,7 @@
   function usePointFromPostcode(postcode, callbackFunction) {
     $.ajax({
       dataType: 'json',
-      url: indiciaData.proxyUrl,
+      url: indiciaData.placeSearchProxyUrl,
       data: {
         url: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
         key: indiciaData.google_api_key,
