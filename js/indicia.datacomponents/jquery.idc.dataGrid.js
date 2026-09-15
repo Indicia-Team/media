47597e1736483e44a65641029f23f68e52f56d1f
John van Breda <john.vanbreda@biodiverseit.co.uk>
2026-09-15 14:16:33 +0100

    DataGrid style tweak
    BEtter rule targeting, so offline verification buttons added to the footer aren't polluted.

--- js/indicia.datacomponents/jquery.idc.dataGrid.js
+++ js/indicia.datacomponents/jquery.idc.dataGrid.js
@@ -968,7 +968,7 @@
         totalCols = el.settings.columns.length
           + (el.settings.responsive ? 1 : 0)
           + (el.settings.actions.length > 0 ? 1 : 0);
-        $('<tfoot><tr class="footer"><td colspan="' + totalCols + '"><div class="form-inline">' + indiciaFns.getFooterControls(el) + '</div></td></tr></tfoot>').appendTo(table);
+        $('<tfoot><tr class="footer"><td colspan="' + totalCols + '"><div class="form-inline inline-pager">' + indiciaFns.getFooterControls(el) + '</div></td></tr></tfoot>').appendTo(table);
       }
       setTableHeight(el);
       // Add tool icons for table settings, full screen and multiselect mode.
